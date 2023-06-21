d583026d56b80710b2875cb46a59aa1b91f35452
RebecaAde <128417129+RebecaAde@users.noreply.github.com>
2023-06-21 16:11:31 +0000

    Deploying to gh-pages from @ RebecaAde/parcel-project-template@90bfb124c8cf93435a53c59b66d2c08960e32886 🚀

---
+++ index.48bbccfb.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll("[data-open-modal]"),o=document.querySelectorAll("[data-close-modal]"),c=document.querySelectorAll("[data-modal]");e.forEach((function(e,o){e.addEventListener("click",(function(){c[o].showModal()}))})),o.forEach((function(e,o){e.addEventListener("click",(function(){c[o].close()}))}))}();
+//# sourceMappingURL=index.48bbccfb.js.map
---
+++ index.f6db5c67.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll("[data-open-modal]"),l=document.querySelectorAll("[data-close-modal]"),o=document.querySelectorAll("[data-modal]");e.forEach(((e,l)=>{e.addEventListener("click",(()=>{o[l].showModal()}))})),l.forEach(((e,l)=>{e.addEventListener("click",(()=>{o[l].close()}))}));
+//# sourceMappingURL=index.f6db5c67.js.map
